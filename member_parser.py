f739d9d0be088772423909aa75f128e1e74cb7f8
YukiKadosaki <yuki1237@i.softbank.jp>
2020-02-26 15:11:28 +0900

    @でメンバーを受けとることができる関数です

--- member_parser.py
+++ member_parser.py
@@ -1,13 +1,12 @@
 import re
 
 def member_parser(text):
-    # return re.findall(r'^目的地[ 　:]*(.*)$', text)[0]
-    return re.findall(r'@([A-z0-9]*)', text)
+   return re.findall(r'@([A-z0-9!#$%&()=^,./?+*]+)', text)
 
 
 
 
 if __name__ == "__main__":
     print("[ Test ]")
-    s = "@rkun20 @Akira @akata @yuki"
+    s = "@rkun!#$%&=^,./?+* @Akira() @akata/? @yuki,."
     print(member_parser(s))
