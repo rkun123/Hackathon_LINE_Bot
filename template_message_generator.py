86547204caf6db8ce89f6ac1512efeaa7ad8b4e9
rkun <rkun123@hotmail.com>
2020-02-27 07:16:02 +0900

    到着時間入力をFlexMessage仕様に変更

--- template_message_generator.py
+++ template_message_generator.py
@@ -17,26 +17,23 @@ def arrival_datepicker():
     now_date = datetime.datetime.now().isoformat()
     regex  = re.findall(r"[0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}:[0-9]{2}", now_date)
     tstr = regex[0] + "T" + regex[1]
-    # Signatureチェック等
-    date_picker = TemplateSendMessage(
-        alt_text="予定日を設定",
-        template=ButtonsTemplate(
-            text="予定日を設定",
-            title="集合日時を設定",
-            actions=[
-                DatetimePickerTemplateAction(
-                    label="設定",
-                    data="action=buy&itemid=2&mode=datetime",
-                    mode="datetime",
-                    initial=tstr,
-                    min=tstr,
-                    max="2099-12-31T23:59"
-                )
-            ]
-        )
-    )
 
-    return date_picker
+    return FlexSendMessage("到着時間", BubbleContainer(
+        size="micro",
+        body=BoxComponent(
+            layout="vertical",
+            contents=[ TextComponent(text="到着時間ボタン") ]
+        ),
+        action=DatetimePickerTemplateAction(
+            label="設定",
+            data="action=buy&itemid=2&mode=datetime",
+            mode="datetime",
+            initial=tstr,
+            min=tstr,
+            max="2099-12-31T23:59"
+        ),
+        styles=BubbleStyle(body=BlockStyle(background_color="#7dfffb"))
+    ))
 
 def arrival_button():
     return FlexSendMessage("到着", BubbleContainer(
