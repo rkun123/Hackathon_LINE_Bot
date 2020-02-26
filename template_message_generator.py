0664ed5cffb1a93dcd0754321cbadd8aa694500c
akiraf0421 <akiraf0421@gmail.com>
2020-02-27 00:57:19 +0900

    develop template_message_generator.py

--- template_message_generator.py
+++ template_message_generator.py
@@ -1,17 +1,22 @@
-def template_message_generator():
+import re, datetime
+
+def templateMessageGenerator():
+    now_date = datetime.datetime.now().isoformat()
+    regex  = re.findall(r'[0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}:[0-9]{2}', now_date)
+    tstr = regex[0] + 'T' + regex[1]
     # Signatureチェック等
     date_picker = TemplateSendMessage(
         alt_text='予定日を設定',
         template=ButtonsTemplate(
             text='予定日を設定',
-            title='YYYY-MM-dd H:M',
+            title='集合日時を設定',
             actions=[
                 DatetimePickerTemplateAction(
                     label='設定',
                     data='action=buy&itemid=2&mode=datetime',
                     mode='datetime',
-                    initial='2017-04-01T10:00',
-                    min='2017-04-01T00:00',
+                    initial=tstr,
+                    min=tstr,
                     max='2099-12-31T23:59'
                 )
             ]
